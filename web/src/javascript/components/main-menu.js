5425b7c3a2956d490eb1b5c4a92c34cba95f1f48
Scott Benes <sbenes@vipasolutions.com>
2015-11-03 15:30:38 -0600

    Remove files no longer needed

--- web/src/javascript/components/main-menu.js
+++
@@ -1,5 +0,0 @@
-jQuery(function($){
-  $('#main_nav > .menu > .mi > .menuitemlabel > .mil').each(function(){
-    $(this).html($(this).text().replace(/^([^\s]+)/, "<span class=\"hl\">$1</span>"));
-  });
-});
